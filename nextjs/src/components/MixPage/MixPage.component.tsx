73d32e362f3ee48ef144401c33b425888b8a8c03
Jan Bladt <jan.bladt@syspons.com>
2024-04-13 23:07:02 +0200

    add play icon

--- nextjs/src/components/MixPage/MixPage.component.tsx
+++ nextjs/src/components/MixPage/MixPage.component.tsx
@@ -2,6 +2,7 @@ import { MixData } from '@app/dummy_data';
 import styles from '@components/MixPage/MixPage.module.css';
 import Image from 'next/image';
 import { PropsWithChildren } from 'react';
+import PlayArrowRoundedIcon from '@mui/icons-material/PlayArrowRounded';
 
 export type MixPageProps = PropsWithChildren<{
   id: string;
@@ -21,6 +22,11 @@ export default function MixPage({id, data}: MixPageProps) {
           width: '100%',
           height: '100vh',
         }}/>
+      <div className={styles.play_container}>
+        <button className={styles.play} onClick={undefined}>
+          <PlayArrowRoundedIcon  style={{fontSize: 200}}/>
+        </button>
+      </div>
       <div className={styles.title_container}>
         <div className={styles.title}>
           <h2>{data.text}</h2>
