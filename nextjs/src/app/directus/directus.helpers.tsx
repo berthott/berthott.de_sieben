38fe26198ef69ac00826184a1d1e9d5635597512
Jan Bladt <jan.bladt@syspons.com>
2024-04-16 18:31:16 +0200

    add dummy directus connection

---
+++ nextjs/src/app/directus/directus.helpers.tsx
@@ -0,0 +1,10 @@
+import { createDirectus, rest, readItems } from '@directus/sdk';
+import { CustomDirectusTypes, Mixes } from './directus';
+
+const client = createDirectus<CustomDirectusTypes>(process.env.NEXT_PUBLIC_BACKEND_URL || '').with(rest());
+
+export async function getMixes(): Promise<Mixes[]> {
+  return  await client.request(
+    readItems('mixes')
+  ) as any as Promise<Mixes[]>;
+}
--- nextjs/src/app/page.tsx
+++ nextjs/src/app/page.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import { MixPage } from '@components/MixPage';
 import { NavBar } from '@components/NavBar';
 import { dummy_data } from './dummy_data';
@@ -5,8 +7,18 @@ import { MenuContextProvider } from '@components/Menu/Menu.state';
 import PicturePage from '@components/PicturePage/PicturePage.component';
 import { Player } from '@components/Player';
 import { PlayerContextProvider } from '@components/Player/Player.state';
+import { useEffect, useState } from 'react';
+import { getMixes } from './directus/directus.helpers';
+import { Mixes } from './directus/directus';
 
 export default function Home() {
+  const [mixes, setMixes] = useState<Mixes[]>([]);
+
+  useEffect(() => {
+    getMixes().then(mixes => setMixes(mixes));
+  }, []);
+
+  console.log('mixes', mixes);
   return (
     <>
       <MenuContextProvider>
