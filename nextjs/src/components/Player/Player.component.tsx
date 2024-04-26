7e206ce17efa528734416f2153cf4c2053e55c4e
Jan Bladt <jan.bladt@syspons.com>
2024-04-26 08:36:39 +0200

    adapt player layout for mobile

--- nextjs/src/components/Player/Player.component.tsx
+++ nextjs/src/components/Player/Player.component.tsx
@@ -8,15 +8,15 @@ import { Fade } from '@components/Fade';
 import PlayArrowIcon from '@mui/icons-material/PlayArrow';
 import PauseIcon from '@mui/icons-material/Pause';
 import VolumeUpIcon from '@mui/icons-material/VolumeUp';
-import DownloadIcon from '@mui/icons-material/Download';
 import Image from 'next/image';
 import { useAppDispatch, useAppSelector } from '@store/store';
 import { Mixes, Track, getMixByKey } from '@directus/mix.model';
-import { assetsUrl, downloadUrl } from '@directus/directus.helpers';
-import { useEffect, useState } from 'react';
+import { assetsUrl } from '@directus/directus.helpers';
+import { useCallback, useEffect, useLayoutEffect, useRef, useState } from 'react';
 import usePlayer from './Player.hook';
 import Slider from '@mui/material/Slider';
 import { Popover, useTheme } from '@mui/material';
+import { useBreakpoints } from '@utils/Breakpoints.hook';
 
 export type PlayerProps = {
   mixes: Mixes;
@@ -26,6 +26,8 @@ export default function Player({ mixes }: PlayerProps) {
   const player = useAppSelector(state => state.player);
   const dispatch = useAppDispatch();
   const mix = getMixByKey(mixes, player.currentlyPlaying!);
+
+  const { isMd } = useBreakpoints();
 
   const [audioSliderValue, setAudioSliderValue] = useState<number | null>(null);
   const [popoverAnchor, setPopoverAnchor] = useState<HTMLButtonElement | null>(null);
@@ -82,7 +84,7 @@ export default function Player({ mixes }: PlayerProps) {
         dispatch(playerActions.resetPlayingState());
       }
     }
-  }, [audioInitialized, mix, play, isCurrentSrc]);
+  }, [audioInitialized, mix, play, isCurrentSrc, dispatch]);
 
   useEffect(() => {
     if (audioInitialized && player.playAt) {
@@ -92,7 +94,7 @@ export default function Player({ mixes }: PlayerProps) {
       }
       dispatch(playerActions.resetPlayingState());
     }
-  }, [audioInitialized, play, player.playAt]);
+  }, [audioInitialized, play, player.playAt, dispatch, playing, stringToDuration, setPlayPosition]);
 
   const getCurrentTrack = (): Track | null => {
     if (!mix || !mix.parsed_tracklist || !currentTime || !mix.parsed_tracklist[0].time) {
@@ -103,80 +105,106 @@ export default function Player({ mixes }: PlayerProps) {
     return currentTrack || mix.parsed_tracklist?.[0];
   }
 
+  const [scrollDistance, setScrollDistance] = useState(0);
+  const metaRef = useCallback((node: HTMLDivElement) => {
+    if (node && node.scrollWidth > node.clientWidth) {
+      setScrollDistance(node.scrollWidth - node.clientWidth);
+    }
+  }, []);
+
   const currentTrack = getCurrentTrack();
 
-  return (
-    mix && <div className={`${styles.player} ${!player.show ? styles.slide_out : ''}`}>
+  const largeIconSize = isMd ? 60 : 40;
+  const smallIconSize = isMd ? 40 : 30;
+  const imageSize = isMd ? '6rem' : '5rem';
+
+  const topIcons = (
+    <>
       <button onClick={() => {
           dispatch(playerActions.hide());
           play(false);
         }}>
-        <CloseIcon style={{fontSize: 60}}/>
+        <CloseIcon style={{fontSize: largeIconSize}}/>
       </button>
       <button onClick={() => document.querySelector(`#${player.currentlyPlaying}_tracklist`)?.scrollIntoView({ behavior: 'smooth', block: 'center' })}>
-        <QueueMusicIcon style={{fontSize: 60}}/>
+        <QueueMusicIcon style={{fontSize: largeIconSize}}/>
       </button>
-      <a href={downloadUrl(mix.audio)}>
-        <DownloadIcon style={{fontSize: 40}}/>
-      </a>
-      <button onClick={event => setPopoverAnchor(event.currentTarget)}>
-        <VolumeUpIcon style={{fontSize: 40}}/>
-      </button>
-      <Popover
-        id='volume'
-        open={Boolean(popoverAnchor)}
-        anchorEl={popoverAnchor}
-        onClose={() => setPopoverAnchor(null)}
-        anchorOrigin={{
-          vertical: -28,
-          horizontal: 'center',
-        }}
-        transformOrigin={{
-          vertical: 'bottom',
-          horizontal: 'center',
-        }}
-        classes={{ paper: styles.volume_popover }}>
-        <Slider 
-          orientation='vertical' 
-          defaultValue={100}
-          onChange={(_, value) => setVolume(value as number / 100)}
-          sx={{
-            height: '90%',
-            ...sliderStyle
-          }}/>
-      </Popover>
-      <div className={styles.waveform}>
-        <span className={styles.title}>{mix.title}</span> 
-        <div className={styles.waveform_meta}>
-          <span className={styles.currentTrack}>{currentTrack ? `${currentTrack.artist} - ${currentTrack.title}` : ''}</span>
-          <span className='grow'></span>
-          <span className={styles.duration}>{`${currentTimeString} / ${durationString}`}</span>
-        </div>
-        <Slider 
-          value={audioSliderValue !== null ? audioSliderValue : currentTime}
-          onChange={(_, value) => setAudioSliderValue(value as number)}
-          onChangeCommitted={(_, value) => setPlayPosition(value as number)}
-          valueLabelDisplay='auto'
-          valueLabelFormat={value => durationToString(value as number)}
-          max={duration}
-          sx={sliderStyle}/>
+    </>
+  );
+
+  return (
+    mix && <div className={`${styles.player} ${!player.show ? styles.slide_out : ''}`}>
+      <div className={styles.player_top}>
+        {!isMd && topIcons}
+        <span className='grow'></span>
       </div>
-      <button onClick={() => play(!playing)}>
-        <Fade in={!playing} states={{
-          a: <PauseIcon style={{fontSize: 60}}/>,
-          b: <PlayArrowIcon style={{fontSize: 60}}/>,
-        }}/>
-      </button>
-      {player.currentlyPlaying && <Image 
-        className={styles.img} 
-        src={assetsUrl(mix.image)} 
-        alt={player.currentlyPlaying || 'preview'} 
-        width={0}
-        height={0} 
-        sizes="6rem" style={{
-          width: '6rem',
-          height: '6rem',
-        }}/>}
+      <div className={styles.player_main}>
+        {isMd && topIcons}
+        <button onClick={event => setPopoverAnchor(event.currentTarget)}>
+          <VolumeUpIcon style={{fontSize: smallIconSize}}/>
+        </button>
+        <Popover
+          id='volume'
+          open={Boolean(popoverAnchor)}
+          anchorEl={popoverAnchor}
+          onClose={() => setPopoverAnchor(null)}
+          anchorOrigin={{
+            vertical: isMd ? -28 : -65,
+            horizontal: 'center',
+          }}
+          transformOrigin={{
+            vertical: 'bottom',
+            horizontal: 'center',
+          }}
+          classes={{ paper: styles.volume_popover }}>
+          <Slider 
+            orientation='vertical' 
+            defaultValue={100}
+            onChange={(_, value) => setVolume(value as number / 100)}
+            sx={{
+              height: '90%',
+              ...sliderStyle
+            }}/>
+        </Popover>
+        <div className={styles.waveform}>
+          <div 
+              ref={!isMd ? metaRef : undefined} 
+              className={`${styles.title} ${scrollDistance ? styles.scroll : ''}`}
+              // @ts-ignore
+              style={!isMd && scrollDistance ? {'--scroll-distance':`-${scrollDistance}px`} : {}}
+              >
+            <span>{mix.title}{currentTrack ? ` - ${currentTrack.artist} - ${currentTrack.title}` : ''}</span> 
+          </div>
+          <div className={styles.slider}>
+            <span className={styles.duration}>{currentTimeString}</span>
+            <Slider 
+              value={audioSliderValue !== null ? audioSliderValue : currentTime}
+              onChange={(_, value) => setAudioSliderValue(value as number)}
+              onChangeCommitted={(_, value) => setPlayPosition(value as number)}
+              valueLabelDisplay='auto'
+              valueLabelFormat={value => durationToString(value as number)}
+              max={duration}
+              sx={sliderStyle}/>
+            <span className={styles.duration}>{durationString}</span>
+          </div>
+        </div>
+        <button onClick={() => play(!playing)}>
+          <Fade in={!playing} states={{
+            a: <PauseIcon style={{fontSize: largeIconSize}}/>,
+            b: <PlayArrowIcon style={{fontSize: largeIconSize}}/>,
+          }}/>
+        </button>
+        {player.currentlyPlaying && <Image 
+          className={styles.img} 
+          src={assetsUrl(mix.image)} 
+          alt={player.currentlyPlaying || 'preview'} 
+          width={0}
+          height={0} 
+          sizes={imageSize} style={{
+            width: imageSize,
+            height: imageSize,
+          }}/>}
+      </div>
     </div>
   );
 }
--- nextjs/tailwind.config.ts
+++ nextjs/tailwind.config.ts
@@ -11,6 +11,15 @@ const config: Config = {
       fontFamily: {
         sans: ['var(--font-berthott)']
       },
+      keyframes: {
+        'scrolling-text': {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(var(--scroll-distance))' },
+        },
+      },
+      animation: {
+        'scrolling-text': 'scrolling-text 15s linear infinite alternate',
+      },
     },
   },
   plugins: [],
