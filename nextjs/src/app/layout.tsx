f9dbd89cc7b52c30e2d3babfd473121fe0a1e141
Jan Bladt <jan.bladt@syspons.com>
2024-06-19 10:56:31 +0200

    add open graph data to metadata

--- nextjs/src/app/layout.tsx
+++ nextjs/src/app/layout.tsx
@@ -3,7 +3,8 @@ import { Inter } from 'next/font/google';
 import './globals.css';
 import { StoreProvider } from '@store/store.provider';
 import { PropsWithChildren } from 'react';
-import { DirectusHelper } from '@directus/directus.helpers';
+import { DirectusHelper, assetTransform } from '@directus/directus.helpers';
+import { headers } from 'next/headers';
 
 const font = Inter({ 
   subsets: ['latin'],
@@ -11,11 +12,37 @@ const font = Inter({
 });
 
 export async function generateMetadata(): Promise<Metadata> {
+  const pathname = headers().get('pathname') || '';
+  const url = headers().get('referer') || '';
   const global = await DirectusHelper.instance().loadGlobal();
-  return {
+  const mix = await DirectusHelper.instance().getCurrentMix(pathname);
+
+
+  const metadata = {
     title: global.title,
     description: global.description,
+    openGraph: {
+      type: 'website',
+      url,
+      title: mix ? `berthott - ${mix.title}, ${global.title}` : global.title || '',
+      description: mix ? `${mix.release}\n\n${mix.tracklist}` : global.description || '',
+      ...( mix ? {images: [512].map(size =>
+        ({ 
+          url: assetTransform(mix.image, {
+            fit: 'cover', 
+            format: 'png',
+            width: size.toString(),
+            height: size.toString(),
+          }),
+          width: size,
+          height: size, 
+          alt: mix.title || '', 
+        })
+      ),} : {}),
+    },
   };
+
+  return metadata;
 }
 
 export default function RootLayout({ children }: PropsWithChildren) {
