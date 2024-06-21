05079c5b7d4421055f9c89211a957deeda0d943f
Jan Bladt <jan.bladt@syspons.com>
2024-06-21 11:08:41 +0200

    fix scrolling behavior in search view

--- nextjs/src/components/search/search.component.tsx
+++ nextjs/src/components/search/search.component.tsx
@@ -6,10 +6,13 @@ import { searchActions } from './search.store';
 import { playerActions } from '@components/player/player.store';
 import { Mix, Mixes, Track } from '@directus/mix.model';
 import { useAppDispatch, useAppSelector } from '@store/store';
+import { useBreakpoints } from '@utils/hooks/breakpoints.hook';
 
 import PlayArrowRoundedIcon from '@mui/icons-material/PlayArrowRounded';
+import CloseIcon from '@mui/icons-material/Close';
 import { debounce } from 'lodash';
 import { ChangeEvent } from 'react';
+import * as noScroll from 'no-scroll';
 
 export type SearchProps = {
   mixes: Mixes;
@@ -23,6 +26,7 @@ type Result = {
 export function Search({ mixes }: SearchProps) {
   const search = useAppSelector(state => state.search);
   const dispatch = useAppDispatch();
+  const { isMd } = useBreakpoints();
 
   // search for tracks that match the search string
   const results = mixes.reduce((r, mix) => {
@@ -31,34 +35,40 @@ export function Search({ mixes }: SearchProps) {
     return found ? r.concat(found) : r;
   }, [] as Result[])
 
+  // disable scrolling when search is open
+  search.show ? noScroll.on() : noScroll.off();
+
   // debounce the search input
   const onChange = debounce((e: ChangeEvent<HTMLInputElement>) => dispatch(searchActions.setSearch(e.target.value)), 250);
   
   return search.show && (
-    <div className={styles.search} onClick={() => dispatch(searchActions.hide())}>
-      <div className={styles.container}>
-        <input 
-          autoFocus 
-          className={styles.input} 
-          type="text" 
-          placeholder='search ...' 
-          onClick={e => e.stopPropagation()}
-          onKeyDown={e => e.key === 'Escape' && dispatch(searchActions.hide())}
-          onChange={e => onChange(e)}/>
-        <ul className={styles.results}>
-          {results.map(result => 
-            <li key={`${result.track.artist} - ${result.track.title}`} className={styles.result}>
-              <div>
-                <div className={styles.artist}>{result.track.artist}</div>
-                <div className={styles.title}>{result.track.title} - <a onClick={() => document.querySelector(`#${result.mix.key}`)?.scrollIntoView({ behavior: 'smooth' })}>{result.mix.title}</a></div>
-              </div>
-              <span className="grow"></span>
-              {result.track.time && (<button onClick={() => dispatch(playerActions.play({ currentlyPlaying: result.mix.key, playAt: result.track.time }))}>
-                <PlayArrowRoundedIcon/>
-              </button>)}
-            </li>)}
-        </ul>
+    <div className={styles.search} onClick={() => dispatch(searchActions.hide())} onWheel={event => event.stopPropagation()}>
+      <div className={styles.top}>
+        <button>
+          <CloseIcon style={{fontSize: isMd ? 60 : 40}}/>
+        </button>
       </div>
+      <input 
+        autoFocus 
+        className={styles.input} 
+        type="text" 
+        placeholder='search ...' 
+        onClick={e => e.stopPropagation()}
+        onKeyDown={e => e.key === 'Escape' && dispatch(searchActions.hide())}
+        onChange={e => onChange(e)}/>
+      <ul className={styles.results}>
+        {results.map(result => 
+          <li key={`${result.track.artist} - ${result.track.title}`} className={styles.result}>
+            <div>
+              <div className={styles.artist}>{result.track.artist}</div>
+              <div className={styles.title}>{result.track.title} - <a onClick={() => document.querySelector(`#${result.mix.key}`)?.scrollIntoView({ behavior: 'smooth' })}>{result.mix.title}</a></div>
+            </div>
+            <span className="grow"></span>
+            {result.track.time && (<button onClick={() => dispatch(playerActions.play({ currentlyPlaying: result.mix.key, playAt: result.track.time }))}>
+              <PlayArrowRoundedIcon/>
+            </button>)}
+          </li>)}
+      </ul>
     </div>
   );
 }
